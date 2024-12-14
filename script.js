f71cc741a784a8beba3ab4c0f78a91d0028cf948
mariam-abubakr-alwakil <mariemelwakil90@gmail.com>
2024-12-14 19:58:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToShop() {
+    document.getElementById("shop").scrollIntoView({ behavior: "smooth" });
+}
+
+function addToCart() {
+    alert("we added your product to the cart");
+}
